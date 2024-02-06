8174a6e57d6afd48b0366f3bf1861f8afd6564f5
gaxencemery@gmail.com <gaxencemery@gmail.com>
2024-02-06 15:30:20 +0000

    margin per order

--- models/intermediate/int_sales_margin.sql
+++ models/intermediate/int_sales_margin.sql
@@ -1,5 +1,8 @@
 SELECT 
     orders_id
+    ,date_date
+    ,revenue
+    ,quantity
     ,round(purchase_price*quantity,2) as purchase_cost
     ,round(revenue - (purchase_price*quantity),2) as margin
 FROM {{ ref('stg_gz_raw__sales') }}
